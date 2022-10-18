cc2f708ceb670d2d95250c78a01eb91a826a5718
anhp4561 <anh-tuan.pham@polymtl.ca>
2022-10-18 09:32:14 -0400

    Update BinarySearchTree.java

--- INF2010-TP3/INF2010-TP3/src/main/java/BinarySearchTree.java
+++ INF2010-TP3/INF2010-TP3/src/main/java/BinarySearchTree.java
@@ -6,7 +6,17 @@ public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> imp
 
     protected BinaryNode<T> add(T value, BinaryNode<T> curNode) {
         //TODO
-        return null;
+        if (curNode == null)
+            return new BinaryNode<T>(value);
+        int compareResult = value.compareTo(curNode.value);
+        if (compareResult < 0)
+            curNode.left = add (value, curNode.left);
+        else if (compareResult > 0)
+            curNode.right = add (value, curNode.right);
+        else
+            ;
+
+        return curNode;
     }
 
     @Override
@@ -26,7 +36,19 @@ public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> imp
 
     protected BinaryNode<T> remove(T value, BinaryNode<T> curNode) {
         //TODO
-        return null;
+        if (curNode == null)
+            return curNode;
+        int compareResult = value.compareTo(curNode.value);
+
+        if (compareResult < 0)
+            curNode.left = remove(value, curNode.left);
+        else if (compareResult > 0)
+            curNode.right = remove(value, curNode.right);
+        else if (curNode.left != null && curNode.right != null) {
+            curNode.value = findMin(curNode.right).value;
+            curNode.right = remove(curNode.value, curNode.right);
+        }
+        return curNode;
     }
 
     protected BinaryNode<T> findMin(BinaryNode<T> curNode){
